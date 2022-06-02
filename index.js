35ed5aaf717c35058f8946bbfdd49c432ee46f05
Nick <nbuscem1@binghamton.edu>
2022-06-02 00:23:09 -0400

    commit tree c00ecd60ccc9f808de0138cbe0a8fe079a7be474

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let finalArray = [];
+    for (let i = 0; i < array.length; i++){
+        finalArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return finalArray
+}
+console.log(writeCards(["Nick", "Claudia", "Maverick"], "Birthday"));
+
+function countDown (num) {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
+countDown(10);
